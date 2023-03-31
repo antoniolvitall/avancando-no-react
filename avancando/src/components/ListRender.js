01ba31b7b6dd94ae51f56af857dad4e05ef2c317
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-31 17:53:46 -0300

    testando lista de renderizacao

--- avancando/src/components/ListRender.js
+++ avancando/src/components/ListRender.js
@@ -3,11 +3,23 @@ import {useState} from 'react'
 const ListRender = () => {
     const [list] = useState(["Vasco", "Gael", "Martim", "Aurora"]);
 
+    const [users] = useState([
+        {id: 1, name: "Vasco", age: 0},
+        {id: 2, name: "Gael", age: 1},
+        {id: 3, name: "Martim", age: 2},
+        {id: 4, name: "Aurora", age: 3},
+    ]);
+
   return (
     <div>
         <ul>
-            {list.map((item) => (
-                <li>{item}</li>
+            {list.map((item, i) => (
+                <li key={i}>{item}</li>
+            ))}
+        </ul>
+        <ul>
+            {users.map((user) => (
+                <li key={user.id}>{user.name} - {user.age}</li>
             ))}
         </ul>
     </div>
