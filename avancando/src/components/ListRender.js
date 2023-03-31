3ef83b2bf281116aba7caaffa850ceb76908402f
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-31 18:25:25 -0300

    testando previus state

--- avancando/src/components/ListRender.js
+++ avancando/src/components/ListRender.js
@@ -1,14 +1,21 @@
 import {useState} from 'react'
 
 const ListRender = () => {
-    const [list] = useState(["Vasco", "Gael", "Martim", "Aurora"]);
+    const [list] = useState(["Vasco", "Gael", "Martim",]);
 
-    const [users] = useState([
+    const [users, setUsers] = useState([
         {id: 1, name: "Vasco", age: 0},
         {id: 2, name: "Gael", age: 1},
         {id: 3, name: "Martim", age: 2},
-        {id: 4, name: "Aurora", age: 3},
     ]);
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            return prevUsers.filter((user) => randomNumber !== user.id);
+        });
+    };
 
   return (
     <div>
@@ -22,6 +29,7 @@ const ListRender = () => {
                 <li key={user.id}>{user.name} - {user.age}</li>
             ))}
         </ul>
+        <button onClick={deleteRandom}> Deletar usuario aleatorio</button>
     </div>
   )
 }
