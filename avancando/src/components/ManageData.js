afd5e47d86dd41ff475fda33f9aaa17a04d4f740
Antonio Lindo <antoniolvitall@gmail.com>
2023-04-01 21:26:48 -0300

    .

--- avancando/src/components/ManageData.js
+++ avancando/src/components/ManageData.js
@@ -10,11 +10,11 @@ const ManageData = () => {
     <div>
         <div>
             <p>Valor: {someData}</p>
-            <button onClick={() => (someData = 15)}>Mudar variavel</button>
+            <button onClick={() => (someData = 15)}>Mudar variavel.</button>
         </div>
         <div>
             <p>Valor: {number}</p>
-            <button onClick={() => setNumber(25)}>Mudar state</button>
+            <button onClick={() => setNumber(25)}>Mudar state.</button>
         </div>
     </div>
   )
