eb1935bd0c9d064c20b181548b1056d4817494a4
Antonio Lindo <antoniolvitall@gmail.com>
2023-04-03 19:03:46 -0300

    testando if ternario

--- avancando/src/components/CondicionalRender.js
+++ avancando/src/components/CondicionalRender.js
@@ -3,11 +3,24 @@ import { useState } from "react";
 const CondicionalRender = () => {
 const [x] = useState(true);
 
+const [name, setName] = useState("Ricardo")
+
   return (
     <div>
         <h1>Isso sera exibido?</h1>
         {x && <p>Se x for true, sim!</p>}
         {!x && <p>Agora x e falso!</p>}
+        <h1>If ternario</h1>
+        {name === "Antonio" ? (
+          <div>
+            <p>O nome e Antonio</p>
+          </div>
+        ) : (
+          <div>
+            <p>Nome nao encontrado!</p>
+          </div>
+        )}
+        <button onClick={() => setName("Antonio") }>Clique aqui!</button>
     </div>
   );
 };
