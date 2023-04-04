77ced24ba1ef5fcfe427b76ac5f1242fdc46b321
Antonio Lindo <antoniolvitall@gmail.com>
2023-04-04 19:09:24 -0300

    renderizacao de lista em componente

--- avancando/src/App.js
+++ avancando/src/App.js
@@ -8,8 +8,12 @@ import ShowUserName from './components/ShowUserName';
 import CarDetails from './components/CarDetails';
 
 function App() {
-const name = "Gael";
 const [userName] = useState("Marcela");
+const cars = [
+  {id:1, brand:"Ferrari", color: "Vermelha", newCar: true, km: 0},
+  {id:1, brand:"Jaguar", color: "Preto", newCar: false, km: 20000},
+  {id:1, brand:"Jeep", color: "Verde", newCar: false, km: 30000},
+]
 
   return (
     <div className="App">
@@ -32,8 +36,12 @@ const [userName] = useState("Marcela");
       {/* reaproveitando */}
       <CarDetails brand="Chevrolet" color="vermelho" km={120000} newCar={false} />
       <CarDetails brand="Honda" color="branco" km={0} newCar={true} />
+      {/* loop em array de objetos */}
+      {cars.map((car) => (
+        <CarDetails brand={car.brand} color={car.color} km={car.km} newCar={car.newCar} />
+      ))};
     </div>
   );
-}
+};
 
 export default App;
